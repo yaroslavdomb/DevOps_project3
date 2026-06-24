fe826798a54da274397aa733ace0ac8be141895c
Yaroslav Domb <yaroslav.domb@gmail.com>
2026-06-24 09:34:09 +0300

    BE - fix bug with missed npm package; fix bug with different type of hotelId

--- backend-repo/src/models/Hotel.js
+++ backend-repo/src/models/Hotel.js
@@ -1,7 +1,16 @@
 import mongoose from "mongoose";
 
 const hotelSchema = new mongoose.Schema({
-    hotelId: { type: String, required: true },
+    hotelId: {
+        type: Number,
+        ref: "Hotel",
+        required: true,
+        min: 0,
+        validate: {
+            validator: Number.isInteger,
+            message: "{VALUE} is not an integer value"
+        }
+    },
     name: { type: String, required: true },
     rooms: {
         type: Number,
@@ -15,7 +24,7 @@ const hotelSchema = new mongoose.Schema({
     photo: { type: String },
     description: { type: String },
     location: { type: String },
-    price: { type: Number}
+    price: { type: Number }
 });
 
 export default mongoose.model("Hotel", hotelSchema);
