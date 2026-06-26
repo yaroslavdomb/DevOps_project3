2412d53eda22f3e9163ff0746105be3421c3b48b
Yaroslav Domb <yaroslav.domb@gmail.com>
2026-06-26 15:29:59 +0300

    BE - change db model to allow both local and docker connection

--- backend-repo/src/models/db.js
+++ backend-repo/src/models/db.js
@@ -4,7 +4,8 @@ export const globalRCon = mongoose.createConnection();
 export const globalWCon = mongoose.createConnection();
 
 export function initDatabase() {
-    const uriBase = `${process.env.MONGO_URI}/${process.env.DATA_STORED}?authSource=${process.env.DATA_STORED}&replicaSet=${process.env.REPLICA}`;
+    const dbAddress = `${process.env.DB_HOST}:${process.env.DB_PORT}`;
+    const uriBase = `${dbAddress}/${process.env.DB_NAME}?authSource=${process.env.DB_NAME}&replicaSet=${process.env.REPLICA}`;
 
     globalRCon.openUri(`mongodb://${process.env.READER_NAME}:${process.env.READER_PASS}@${uriBase}`);
     globalWCon.openUri(`mongodb://${process.env.WRITER_NAME}:${process.env.WRITER_PASS}@${uriBase}`);
